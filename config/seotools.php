3a81f23a1b12bc7680806a93602107160462a7f1
yuzgec <olcayy@gmail.com>
2023-04-07 03:17:26 +0300

    Fix

--- config/seotools.php
+++ config/seotools.php
@@ -9,9 +9,9 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "Ermaksan Deri Makinaları Kürk Makinaları", // set false to total remove
-            'titleBefore'  => false, // Put defaults.title before page title, like 'Ermaksan Deri Makinaları Kürk Makinaları - Dashboard'
-            'description'  => 'Ermaksan Deri Makinaları Kürk Makinaları', // set false to total remove
+            'title'        => "FSR Kimya", // set false to total remove
+            'titleBefore'  => false, // Put defaults.title before page title, like 'FSR Kimya - Dashboard'
+            'description'  => 'FSR Kimya', // set false to total remove
             'separator'    => ' - ',
             'keywords'     => [],
             'canonical'    => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
@@ -36,8 +36,8 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Ermaksan Deri Makinaları Kürk Makinaları', // set false to total remove
-            'description' => 'Ermaksan Deri Makinaları Kürk Makinaları', // set false to total remove
+            'title'       => 'FSR Kimya', // set false to total remove
+            'description' => 'FSR Kimya', // set false to total remove
             'url'         => false, // Set null for using Url::current(), set false to total remove
             'type'        => false,
             'site_name'   => false,
@@ -58,8 +58,8 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Ermaksan Deri Makinaları Kürk Makinaları', // set false to total remove
-            'description' => 'Ermaksan Deri Makinaları Kürk Makinaları', // set false to total remove
+            'title'       => 'FSR Kimya', // set false to total remove
+            'description' => 'FSR Kimya', // set false to total remove
             'url'         => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'type'        => 'WebPage',
             'images'      => [],
