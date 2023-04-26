be0b637d896ec515d206de00e2891f764b010ec7
yuzgec <olcayy@gmail.com>
2023-04-26 21:32:50 +0300

    fix

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
+use CyrildeWit\EloquentViewable\Contracts\Viewable;
 use CyrildeWit\EloquentViewable\InteractsWithViews;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Blog extends Model
+class Blog extends Model implements HasMedia,TranslatableContract,Viewable
 {
     use HasFactory,SoftDeletes,InteractsWithMedia,Translatable,InteractsWithViews;
 
