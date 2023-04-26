38e1ec6a4fd5ef860d8794b237fb4055ebba80a1
yuzgec <olcayy@gmail.com>
2023-04-26 23:13:07 +0300

    Fix

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,13 +1,11 @@
 <header class="bixol-header bg-sticky-white">
     <div class="bixol-info-bar">
         <div class="container">
-            <div class="row align-items-center">
-                <div class="col-sm-8">
-                   <div class="bixol-info-left">
-                    <span class="text-white">{{ __('site.firma') }} </span>
-                    </div>
+            <div class="row">
+                <div class="col-sm-2" style="align-items: flex-start">
+                       <img src="/fsrkimya-beyaz-logo.svg" style="height: 25px">
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-10">
                     <div class="bixol-info-social">
                         <a href="#" class="text-white"><i class="fab fa-facebook"></i></a>
                         <a href="#" class="text-white"><i class="fab fa-twitter"></i></a>
@@ -30,6 +28,9 @@
                     <div class="col-lg-6">
                         <div class="bixol-header-top-left">
                             <div class="bixol-header-item">
+                              <img src="/fsr-logo.svg" style="width: 100px;margin-left:-50px" >
+                            </div>
+                            <div class="bixol-header-item">
                                 <span class="bixol-icon-wrapper">
                                     <i class="flaticon flaticon-phone"></i>
                                 </span>
@@ -50,7 +51,7 @@
                             </div>--}}
                         </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-lg-6" >
                         <div class="bixol-header-top-right">
                             <div class="bixol-header-item">
                                         <span class="bixol-icon-wrapper">
@@ -82,7 +83,7 @@
         <div class="container">
             <div class="bixol-nav">
                 <a href="{{ route('home') }}" class="bixol-header-logo">
-                    <img src="/logo.svg" alt="{{ config('app.name') }}">
+                    <img src="drshield.png" alt="{{ config('app.name') }}" >
                 </a>
                 <nav class="desktop-menu">
                     <ul>
@@ -118,7 +119,7 @@
                 <div class="row align-items-center">
                     <div class="col-6">
                         <a href="{{ route('home') }}" class="bixol-logo">
-                            <img src="/logo.svg" alt="{{ __('site.firma') }}">
+                            <img src="/fsr-logo.svg" alt="{{ __('site.firma') }}">
                         </a>
                     </div>
                     <div class="col-6">
