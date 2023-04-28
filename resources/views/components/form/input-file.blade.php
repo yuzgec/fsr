864c2e7a0d12fdced3072286e037c17d0b4fc9ea
yuzgec <olcayy@gmail.com>
2023-04-29 02:45:41 +0300

    Fix

--- resources/views/components/form/input-file.blade.php
+++ resources/views/components/form/input-file.blade.php
@@ -1,5 +1,5 @@
 
-{{Form::file($name, ["class" => $class . (($errors->has($name))?" is-invalid":""),'aaccept'=>'image/*'])}}
+{{Form::file($name, ["class" => $class . (($errors->has($name))?" is-invalid":""),'accept'=>'image/*'])}}
 
 @if($errors->has($name))
     <div class="invalid-feedback">{{$errors->first($name)}}</div>
